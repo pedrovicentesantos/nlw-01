c571bef41c2b3e09c1da7cc5c570ce6dcd7e91df
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2021-12-12 17:56:03 -0300

    refactor: add try/catch to ItemsController

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -3,17 +3,19 @@ import knex from '../database/connection';
 
 class ItemsController {
   async index(request: Request, response: Response) {
-    const items = await knex('items').select('*');
-  
-    const serializedItems = items.map(item => {
-      return {
-        id: item.id,
-        title: item.title,
-        image_url: `http://192.168.0.13:3333/uploads/${item.image}`
-      }
-    });
-    
-    return response.json(serializedItems)
+    try {
+      const items = await knex('items').select('*');
+      const serializedItems = items.map(item => {
+        return {
+          id: item.id,
+          title: item.title,
+          image_url: `http://192.168.0.13:3333/uploads/${item.image}`
+        }
+      });
+      return response.json(serializedItems);
+    } catch (error: any) {
+      return response.status(500).json({ error: error.message });
+    }
   }
 }
 
