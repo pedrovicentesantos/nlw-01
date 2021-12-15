0b7fbcef682f51f3df95165ecb5e5243edee37a5
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2021-12-15 20:04:17 -0300

    chore: pass IP address to env file

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { BASE_URL } from "@env" ;
 
 const api = axios.create({
-  baseURL:'http://192.168.0.13:3333/'
+  baseURL:`http://${process.env.BASE_URL}:3333/`
 });
 
 export default api;
